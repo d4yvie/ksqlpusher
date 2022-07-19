e64584af567a649efcd3162dca2137ca00fd0d2f
David <d4v3z02@gmail.com>
2022-07-19 21:08:55 +0200

    added initDB dummy

--- ksql/org.ksql/src/main/java/org/ksql/Main.java
+++ ksql/org.ksql/src/main/java/org/ksql/Main.java
@@ -3,6 +3,8 @@ package org.ksql;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Collections;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
@@ -13,12 +15,13 @@ import org.apache.commons.csv.CSVRecord;
 
 import io.confluent.ksql.api.client.AcksPublisher;
 import io.confluent.ksql.api.client.Client;
+import io.confluent.ksql.api.client.ExecuteStatementResult;
 import io.confluent.ksql.api.client.InsertsPublisher;
 import io.confluent.ksql.api.client.KsqlObject;
 
 public class Main {
 	
-	private static String TABLE = "DA_TABLE";
+	private static String TABLE = "creditcard_data";
 
 	public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
 		new Main().doIt();
@@ -62,6 +65,24 @@ public class Main {
 			e.printStackTrace();
 			return CompletableFuture.allOf(null);
 		}
+	}
+
+	public void initDB() throws InterruptedException, ExecutionException {
+		String sql = String.format("CREATE STREAM %s (Time BIGINT, Amount DOUBLE, Fraud_check INT)"
+	               + "WITH (KAFKA_TOPIC='check_creditcard', VALUE_FORMAT='json');", TABLE);
+		Client client = KsqlClientFactory.retrieveClient();
+		client.executeStatement(sql).get();
+		// creates new ksqlDB stream with topic
+		client.executeStatement(String.format("DROP TABLE %s;", TABLE)).get();
+		// drop table (name) if exist
+		String sql1 = "CREATE TABLE creditcard_BY_USER AS "
+	               + "SELECT Time, COUNT(*) as COUNT " //, "SELECT Amount, COUNT(*) as COUNT ", "SELECT Fraud_check, COUNT(*) as COUNT "
+	               + String.format("FROM %s GROUP BY Time EMIT CHANGES;", TABLE);
+		Map<String, Object> properties = Collections.singletonMap("auto.offset.reset", "earliest");
+		ExecuteStatementResult result = client.executeStatement(sql1, properties).get();
+		System.out.println("Query ID: " + result.queryId().orElse("<null>"));
+		// starts persistent query with stream from 
+		client.executeStatement("TERMINATE CTAS_ORDERS_BY_USER_0;").get();
 	}
 
 	public boolean handleRecordReactive(KsqlObject record, InsertsPublisher insertsPublisher) {
