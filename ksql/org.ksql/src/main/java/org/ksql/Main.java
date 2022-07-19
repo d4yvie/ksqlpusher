45232393be19c6af2efacf100a11c41dbc9edbe5
David <d4v3z02@gmail.com>
2022-07-19 20:49:21 +0200

    added reactive version

--- ksql/org.ksql/src/main/java/org/ksql/Main.java
+++ ksql/org.ksql/src/main/java/org/ksql/Main.java
@@ -5,45 +5,66 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 
+import io.confluent.ksql.api.client.AcksPublisher;
 import io.confluent.ksql.api.client.Client;
-import io.confluent.ksql.api.client.ClientOptions;
+import io.confluent.ksql.api.client.InsertsPublisher;
 import io.confluent.ksql.api.client.KsqlObject;
 
 public class Main {
+	
+	private static String TABLE = "DA_TABLE";
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
 		new Main().doIt();
 	}
 
-	public void doIt() throws IOException {
+	public void doIt() throws IOException, InterruptedException, ExecutionException {
 		Reader in = new FileReader("src/main/resources/creditcard.csv");
 		Iterable<CSVRecord> records = CSVFormat.RFC4180.parse(in);
+		Client client = KsqlClientFactory.retrieveClient();
+		
+		// not reactive handling
 		StreamSupport.stream(records.spliterator(), false)
+			.map(this::recordToObject)
 			.map(this::handleRecord)
 			.collect(CompletableFutureUtil.collectResult());
 
-		Client client = KsqlClientFactory.retrieveClient();
+		// reactive handling
+//		InsertsPublisher insertsPublisher = new InsertsPublisher();
+//		AcksPublisher acksPublisher = client.streamInserts(TABLE, insertsPublisher).get();
+//		StreamSupport.stream(records.spliterator(), false)
+//			.map(this::recordToObject)
+//			.map(record -> this.handleRecordReactive(record, insertsPublisher))
+//			.collect(Collectors.toList());
+//		insertsPublisher.complete();
+
 		client.close();
 	}
 
-	public CompletableFuture<Void> handleRecord(CSVRecord record) {
+	public KsqlObject recordToObject(CSVRecord record) {
 		String columnOne = record.get(0);
 		String columnThree = record.get(29);
 		String columnFour = record.get(30);
-		// System.out.println(String.format("%s %s %s %s", columnOne, columnTwo, columnThree, columnFour));
+		return new KsqlObject().put("Time", columnOne).put("Amount", columnThree).put("Fraud_check", columnFour);
+	}
 
+	public CompletableFuture<Void> handleRecord(KsqlObject row) {
 		Client client = KsqlClientFactory.retrieveClient();
-		KsqlObject row = new KsqlObject().put("Time", columnOne).put("Amount", columnThree).put("Fraud_check", columnFour);
 		try {
-			return client.insertInto("DA_TABLE", row);
+			return client.insertInto(TABLE, row);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return CompletableFuture.allOf(null);
 		}
+	}
+
+	public boolean handleRecordReactive(KsqlObject record, InsertsPublisher acksPublisher) {
+		return acksPublisher.accept(record);
 	}
 }
