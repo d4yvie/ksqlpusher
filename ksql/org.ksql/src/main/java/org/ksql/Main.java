8e1a42bda595d964ac622d4550854de2a3c9fe37
David <d4v3z02@gmail.com>
2022-07-20 21:35:36 +0200

    added running solution with throttling

--- ksql/org.ksql/src/main/java/org/ksql/Main.java
+++ ksql/org.ksql/src/main/java/org/ksql/Main.java
@@ -3,9 +3,7 @@ package org.ksql;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
@@ -14,16 +12,24 @@ import java.util.stream.StreamSupport;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 
+import com.google.common.collect.Iterators;
+import com.google.common.collect.UnmodifiableIterator;
+
 import io.confluent.ksql.api.client.AcksPublisher;
 import io.confluent.ksql.api.client.Client;
-import io.confluent.ksql.api.client.ExecuteStatementResult;
 import io.confluent.ksql.api.client.InsertsPublisher;
 import io.confluent.ksql.api.client.KsqlObject;
+import io.confluent.ksql.api.client.QueryInfo;
+import io.confluent.ksql.api.client.QueryInfo.QueryType;
+import io.confluent.ksql.api.client.StreamInfo;
+import io.confluent.ksql.api.client.TableInfo;
+import io.confluent.ksql.api.client.TopicInfo;
 
 public class Main {
 	
 	private static String TABLE = "creditcard_data";
-	private static int BUFFER_SIZE = 200000;
+	private static int BUFFER_SIZE = 2000000000;
+	private static int TIME_BETWEEN_REQUEST_IN_MS = 1;
 
 	public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
 		new Main().doIt();
@@ -32,26 +38,48 @@ public class Main {
 	public void doIt() throws IOException, InterruptedException, ExecutionException {
 		Reader in = new FileReader("src/main/resources/creditcard.csv");
 		Iterable<CSVRecord> records = CSVFormat.RFC4180.parse(in);
+		
+		// sendReactive(records);
+		
+		send(records);
+
+		KsqlClientFactory.retrieveClient().close();
+	}
+	
+	public void sendReactiveBatched(Iterable<CSVRecord> records) throws InterruptedException, ExecutionException { 
+		UnmodifiableIterator<List<CSVRecord>> batchIterator = Iterators.partition(records.iterator(), 200);
 		Client client = KsqlClientFactory.retrieveClient();
 		
-		// not reactive handling
-//		StreamSupport.stream(records.spliterator(), false)
-//			.map(this::recordToObject)
-//			.map(this::handleRecord)
-//			.collect(CompletableFutureUtil.collectResult());
-
-		// reactive handling
+		
+		
 		InsertsPublisher insertsPublisher = new InsertsPublisher(BUFFER_SIZE);
 		AcksPublisher acksPublisher = client.streamInserts(TABLE, insertsPublisher).get();
 		acksPublisher.subscribe(new AcksSubscriber());
 		List<Boolean> successful = StreamSupport.stream(records.spliterator(), false)
 			.map(this::recordToObject)
+			
 			.map(record -> this.handleRecordReactive(record, insertsPublisher))
 			.collect(Collectors.toList());
+	}
+	
+	public void sendReactive(Iterable<CSVRecord> records) throws InterruptedException, ExecutionException { 
+		Client client = KsqlClientFactory.retrieveClient();
+		InsertsPublisher insertsPublisher = new InsertsPublisher(BUFFER_SIZE);
+		AcksPublisher acksPublisher = client.streamInserts(TABLE, insertsPublisher).get();
+		acksPublisher.subscribe(new AcksSubscriber());
+		List<Boolean> successful = StreamSupport.stream(records.spliterator(), false)
+			.map(this::recordToObject)
+			
+			.map(record -> this.handleRecordReactive(record, insertsPublisher))
+			.collect(Collectors.toList());
+	}
 
-		insertsPublisher.complete();
-
-		client.close();
+	public void send(Iterable<CSVRecord> records) throws InterruptedException, ExecutionException { 
+		Client client = KsqlClientFactory.retrieveClient();
+		StreamSupport.stream(records.spliterator(), false)
+			.map(this::recordToObject)
+			.map(record -> this.handleRecord(record))
+			.collect(CompletableFutureUtil.collectResult());
 	}
 
 	public KsqlObject recordToObject(CSVRecord record) {
@@ -64,32 +92,82 @@ public class Main {
 	public CompletableFuture<Void> handleRecord(KsqlObject row) {
 		Client client = KsqlClientFactory.retrieveClient();
 		try {
+			Thread.sleep(TIME_BETWEEN_REQUEST_IN_MS);
 			return client.insertInto(TABLE, row);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return CompletableFuture.allOf(null);
 		}
 	}
-
-	public void initDB() throws InterruptedException, ExecutionException {
-		String sql = String.format("CREATE STREAM %s (Time BIGINT, Amount DOUBLE, Fraud_check INT)"
-	               + "WITH (KAFKA_TOPIC='check_creditcard', VALUE_FORMAT='json');", TABLE);
+	
+	public void runSelect() {
+		String selectQuery = String.format("SELECT * FROM %s EMIT CHANGES;", TABLE);
+		
 		Client client = KsqlClientFactory.retrieveClient();
-		client.executeStatement(sql).get();
-		// creates new ksqlDB stream with topic
-		client.executeStatement(String.format("DROP TABLE %s;", TABLE)).get();
-		// drop table (name) if exist
-		String sql1 = "CREATE TABLE creditcard_BY_USER AS "
-	               + "SELECT Time, COUNT(*) as COUNT " //, "SELECT Amount, COUNT(*) as COUNT ", "SELECT Fraud_check, COUNT(*) as COUNT "
-	               + String.format("FROM %s GROUP BY Time EMIT CHANGES;", TABLE);
-		Map<String, Object> properties = Collections.singletonMap("auto.offset.reset", "earliest");
-		ExecuteStatementResult result = client.executeStatement(sql1, properties).get();
-		System.out.println("Query ID: " + result.queryId().orElse("<null>"));
-		// starts persistent query with stream from 
-		client.executeStatement("TERMINATE CTAS_ORDERS_BY_USER_0;").get();
+		client.streamQuery(selectQuery)
+	    .thenAccept(streamedQueryResult -> {
+	      System.out.println("Query has started. Query ID: " + streamedQueryResult.queryID());
+	      RowSubscriber subscriber = new RowSubscriber();
+	      streamedQueryResult.subscribe(subscriber);
+	    }).exceptionally(e -> {
+	      System.out.println("Request failed: " + e);
+	      return null;
+	    });
+		
+		System.out.println(selectQuery);
+	}
+
+	public void listInfo() throws InterruptedException, ExecutionException {
+		
+		Client client = KsqlClientFactory.retrieveClient();
+		
+		List<StreamInfo> streams = client.listStreams().get();
+		for (StreamInfo stream : streams) {
+		  System.out.println(
+		     stream.getName() 
+		     + " " + stream.getTopic() 
+		     + " " + stream.getKeyFormat()
+		     + " " + stream.getValueFormat()
+		     + " " + stream.isWindowed()
+		  );
+		}
+		
+		List<TableInfo> tables = client.listTables().get();
+		for (TableInfo table : tables) {
+		  System.out.println(
+		       table.getName() 
+		       + " " + table.getTopic() 
+		       + " " + table.getKeyFormat()
+		       + " " + table.getValueFormat()
+		       + " " + table.isWindowed()
+		    );
+		}
+		
+		List<TopicInfo> topics = client.listTopics().get();
+		for (TopicInfo topic : topics) {
+		  System.out.println(
+		       topic.getName() 
+		       + " " + topic.getPartitions() 
+		       + " " + topic.getReplicasPerPartition()
+		  );
+		}
+		
+		List<QueryInfo> queries = client.listQueries().get();
+		for (QueryInfo query : queries) {
+		  System.out.println(query.getQueryType() + " " + query.getId());
+		  if (query.getQueryType() == QueryType.PERSISTENT) {
+		    System.out.println(query.getSink().get() + " " + query.getSinkTopic().get());
+		  }
+		}
 	}
 
 	public boolean handleRecordReactive(KsqlObject record, InsertsPublisher insertsPublisher) {
-		return insertsPublisher.accept(record);
+		try {
+			Thread.sleep(TIME_BETWEEN_REQUEST_IN_MS);
+			return insertsPublisher.accept(record);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			return insertsPublisher.accept(record);
+		}
 	}
 }
