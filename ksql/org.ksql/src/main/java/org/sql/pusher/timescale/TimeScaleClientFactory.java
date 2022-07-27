27ab03c8d54201dfc6598b0b7adc26e45e74f3b6
David <d4v3z02@gmail.com>
2022-07-27 22:15:14 +0200

    added timescale connection with table

--- ksql/org.ksql/src/main/java/org/sql/pusher/timescale/TimeScaleClientFactory.java
+++ ksql/org.ksql/src/main/java/org/sql/pusher/timescale/TimeScaleClientFactory.java
@@ -5,17 +5,35 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class TimeScaleClientFactory {
-	
+
 	private static final String HOST = "localhost";
 	private static final String PORT = "5432";
-	private static final String CONNECTION_URL = String.format("jdbc:postgresql://%s:%s/dbname?user=username&password=password", HOST, PORT);
-	
+	private static final String USER = "postgres";
+	private static final String PASSWORD = "password";
+	private static final String DATABASE_NAME = "postgres";
+	private static final String CONNECTION_URL = "jdbc:postgresql://%s:%s/%s?user=%s&password=%s";
+
 	public static Connection getConnection() {
 		try {
-			return DriverManager.getConnection(CONNECTION_URL);
+			return DriverManager.getConnection(String.format(CONNECTION_URL, HOST, PORT, DATABASE_NAME, USER, PASSWORD));
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
+		}
+	}
+
+	public static void main(String... args) throws SQLException {
+		Connection conn = getConnection();
+		var createSensorTableQuery = """
+				           CREATE TABLE creditcard_data (
+				           	Time VARCHAR PRIMARY KEY,
+				Amount VARCHAR,
+				Fraud_check VARCHAR
+				           )
+				           """;
+
+		try (var stmt = conn.createStatement()) {
+			stmt.execute(createSensorTableQuery);
 		}
 	}
 }
