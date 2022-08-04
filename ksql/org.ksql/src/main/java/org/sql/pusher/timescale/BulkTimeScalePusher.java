9f83ea1e6457b227339cde363353ca55cd6ec71b
David <d4v3z02@gmail.com>
2022-08-04 19:55:02 +0200

    updated transformation for new data set

--- ksql/org.ksql/src/main/java/org/sql/pusher/timescale/BulkTimeScalePusher.java
+++ ksql/org.ksql/src/main/java/org/sql/pusher/timescale/BulkTimeScalePusher.java
@@ -4,11 +4,11 @@ import java.sql.Connection;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import org.apache.commons.csv.CSVRecord;
 import org.sql.pusher.Batcher;
-import org.sql.pusher.ForkJoinExecutor;
 import org.sql.pusher.SqlPusher;
 
 public class BulkTimeScalePusher implements SqlPusher {
@@ -20,10 +20,15 @@ public class BulkTimeScalePusher implements SqlPusher {
 		long t1 = System.nanoTime();
 		System.out.println("STARTING BULK TIMESCALE INSERTS");
 		String queryTemplate = String.format("INSERT INTO %s (Time, Amount, Fraud_check) VALUES ", stream);
-		List<String> values = recordsStream
-				.map(record -> String.format("(%s, %s, %s)", record.get(0), record.get(29), record.get(30)))
+		Stream<Object[]> result = recordsStream.map(val -> val.toList())
+				.map(val -> val.toArray(new String[22])).map(arr -> {
+					return arr;
+				});
+		String valueTemplate = createRowTemplate(20);
+		List<String> values = result
+				.map(record -> String.format(valueTemplate, record))
 				.collect(Collectors.toList());
-		Stream<List<String>> batches = Batcher.ofSubLists(values, 500);
+		Stream<List<String>> batches = Batcher.ofSubLists(values, 1);
 		batches.forEach(batch -> {
 			String concatenatedBatch = batch.stream().reduce("",
 					(a, b) -> a.isEmpty() ? b : String.format("%s, %s", a, b));
@@ -36,5 +41,15 @@ public class BulkTimeScalePusher implements SqlPusher {
 		});
 		System.out.println(String.format("Success! Bulk TimeScale Inserting took %d seconds",
 				TimeUnit.SECONDS.convert(System.nanoTime() - t1, TimeUnit.NANOSECONDS)));
+	}
+
+	private String createRowTemplate(int columns) {
+		return IntStream.range(0, columns).mapToObj(it -> "")
+				.reduce("(%s", (acc, it) -> acc + ", %s") + ")";
+	}
+
+	public static void main(String[] args) {
+		System.out.println(IntStream.range(0, 20).mapToObj(it -> "")
+				.reduce("(%s", (acc, it) -> acc + ", %s")+ ")") ;
 	}
 }
